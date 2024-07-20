6980354a77b8f53851cf2a34d6628209f402d5f9
ituki0426 <yuzurihainori315@gmail.com>
2024-07-20 22:40:23 +0900

    make environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bahdanau_attention',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo for the project Bahdanau_Attention.',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
---
+++ src/core.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+core.py
+
+Created on `2024-07-20
+by Your name (or your organization/company/team)
+yourname@example.com
+"""
+import logging as log
+
+def main():
+    # TODO:
+    # run its processes
+    pass
+
+if __name__ == "__main__":
+    log.basicConfig(level=log.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    datefmt="%b %d %H:%M:%S %Z")
+
+    main()
